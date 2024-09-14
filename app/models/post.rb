906fb3c4807777af2f9f6daf81dfc2522c9b86bf
Pedro Augusto <pedrogusferva@gmail.com>
2024-09-14 08:46:01 -0300

    Finished turbo implementation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ApplicationRecord
   validates :content, presence: true
   belongs_to :user
+
+  after_create_commit -> {
+    broadcast_prepend_to :posts
+  }
+
+  after_update_commit -> {
+    broadcast_update_to :posts
+  }
 end
